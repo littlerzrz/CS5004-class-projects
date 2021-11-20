c184de9ead798b021cde5f177b18b2486c106351
littlerzrz <renxinyi.design@gmail.com>
2021-11-19 17:18:14 -0800

    project5 initial commits

---
+++ project5/src/cs5004/marblesolitaire/model/MarbleSolitaireModelImpl.java
@@ -0,0 +1,39 @@
+package cs5004.marblesolitaire.model;
+
+public class MarbleSolitaireModelImpl implements MarbleSolitaireModel{
+  public MarbleSolitaireModelImpl() {
+
+  }
+
+  public MarbleSolitaireModelImpl(int sRow, int sCol) {
+
+  }
+
+  public MarbleSolitaireModelImpl(int armThickness) {
+
+  }
+
+  public MarbleSolitaireModelImpl(int armThickness, int sRow, int sCol) {
+
+  }
+
+  @Override
+  public void move(int fromRow, int fromCol, int toRow, int toCol) throws IllegalArgumentException {
+
+  }
+
+  @Override
+  public boolean isGameOver() {
+    return false;
+  }
+
+  @Override
+  public String getGameState() {
+    return null;
+  }
+
+  @Override
+  public int getScore() {
+    return 0;
+  }
+}
